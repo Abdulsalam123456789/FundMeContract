f54cbbdb76d8adef7db92d38f847ff4264e81ad0
slam <gbotolorunabdulsalam@gmail.com>
2025-04-19 09:32:37 +0100

    Hi, I;m foundey

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import{Test, console} from "forge-std/Test.sol";
+//import{stdCheats} from "forge-std/StdCheats.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
@@ -28,7 +29,7 @@ contract FundMeTest is Test {
     function testOwnnerIsMsgSender() public  {
         //console.log(fundMe.i_owner());
         //console.log(msg.sender);
-        assertEq(fundMe.getOwner(), msg.sender);
+        assertEq(fundMe.getOwner(), address(this));
     }
 
     function testPriceFeedVersionIsAccurate() public {
@@ -121,6 +122,35 @@ contract FundMeTest is Test {
             );
 
     }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        //Arrange
+        uint256 numberOfFunders = 10;
+        uint256 startingFunderIndex =2;
+        for (uint256 i = startingFunderIndex; i < numberOfFunders; i++){
+            //vm.prank new address
+            //vm.deal new address
+            address funder = vm.addr(i);
+            hoax(funder, SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        //Assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+            fundMe.getOwner().balance
+            );
+
+    }
+
 
 }
 
